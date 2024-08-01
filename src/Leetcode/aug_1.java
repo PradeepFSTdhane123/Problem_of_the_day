3ff61ee41a8180d974b45177d84ac457c49a5b25
pradeep dhane <pradeepdhane3@gmail.com>
2024-08-02 02:31:17 +0530

    aug-1 leetcode

---
+++ src/Leetcode/aug_1.java
@@ -0,0 +1,54 @@
+package Leetcode;
+
+
+// // 🏅given
+//     1.[]string of person details fixed string size-15.
+//    🏅Task
+//     1.count the age of person above 60.
+
+public class aug_1 {
+
+//class Solution {
+    public int countSeniors(String[] details) {
+        // 🛑using char method to int.-best optimum solution.
+        int count=0;
+        for(String str:details){
+            int num1=str.charAt(11)-'0';
+            int num2=str.charAt(12)-'0';
+            int age=num1*10+num2;
+            //  👉🏻👉🏻 int age=(s.charAt(11)-'0')*10+s.charAt(12)-'0';
+            if(age>60) count++;
+        }
+        return count;
+    }
+
+
+// 🛑usign substring method to int.  substring methods need time.
+//     int count=0;
+//     for(String str:details){
+//     int age=Integer.parseInt(str.substring(11,13));
+//       if(age>=60){
+//       count++;
+//👉🏻👉🏻or if(Integer.parseInt(str.substring(11,13))>60)count++;
+//        }
+//      }
+//      return count;
+//    }
+
+
+//🙂two methods
+//    A.chaAt
+//     1.charAt the age using index.
+//     2.convert it in int by subtract '0'
+//     3.multiple by 10 to first no. for convet it in tens and unit place add as it is.
+
+//  B.using substring
+//    1.substring the part of string of age.
+//    2.convert sting to int by using Integer.parseInt()
+
+
+public static void main(String[] args) {
+
+}
+
+}
